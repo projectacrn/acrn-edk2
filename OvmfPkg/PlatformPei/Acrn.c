projectacrn/acrn-edk2

commit 408fd4cbae07ff06c6e9ad3166c2071ea2383253

acrn: fix how to get memory below 4G
Fixes: e7c3a00353 ("Use ACRN E820 map to query system memory size")

In determine the range of memory to use during PEI, it assume the memory
below 4G or above 4G is continuous. However, if there's GPU DSM region
(which needs identical map), it would break the low memory into two
parts. In this case, we could let the OVMF see the lowmem part1.

Signed-off-by: Fei Li <fei1.li@intel.com>

OvmfPkg/PlatformPei/Acrn.c
@@ -63,10 +63,14 @@ AcrnGetSystemMemorySizeBelow4gb (
     return RETURN_UNSUPPORTED;
   }
 
-  for (Loop = (INT32)(E820->E820EntriesCount - 1), Entry = &E820->E820Map[Loop];
-       Loop >= 0; Entry = &E820->E820Map[--Loop]) {
-    if (Entry->BaseAddr + Entry->Length < BASE_4GB &&
-        Entry->Type == EfiAcpiAddressRangeMemory) {
+  //
+  // ACRN e820 tables are sorted and the low memory address
+  // is start larger than 1MB
+  //
+  for (loop = 0; loop < E820->E820EntriesCount; loop++) {
+    Entry = &E820->E820Map[Loop];
+    if (Entry->Type == EfiAcpiAddressRangeMemory &&
+        Entry->BaseAddr >= 0x100000)
       *MemSize = Entry->BaseAddr + Entry->Length;
       return RETURN_SUCCESS;
     }

commit 91f9b6efc35c494616d603a16b66a5fb41685484

Ovmf:ACRN: Fix the building error
Fix the building error introduced in
408fd4cbae07ff06c6e9ad3166c2071ea2383253.
    a. Incorrect variable definition
    b. the missing braces

Signed-off-by: Zhao Yakui <yakui.zhao@intel.com>

OvmfPkg/PlatformPei/Acrn.c
@@ -67,10 +67,10 @@ AcrnGetSystemMemorySizeBelow4gb (
   // ACRN e820 tables are sorted and the low memory address
   // is start larger than 1MB
   //
-  for (loop = 0; loop < E820->E820EntriesCount; loop++) {
+  for (Loop = 0; Loop < E820->E820EntriesCount; Loop++) {
     Entry = &E820->E820Map[Loop];
-    if (Entry->Type == EfiAcpiAddressRangeMemory &&
-        Entry->BaseAddr >= 0x100000)
+    if ((Entry->Type == EfiAcpiAddressRangeMemory) &&
+        (Entry->BaseAddr >= 0x100000)) {
       *MemSize = Entry->BaseAddr + Entry->Length;
       return RETURN_SUCCESS;
     }